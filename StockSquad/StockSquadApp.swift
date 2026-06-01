8bafb51495dc1c9a39257e8c5a592d6056009db9
Tmwakalasya <mwakalasyatuntu@gmail.com>
2026-06-01 10:53:29 -0400

    Initial Commit

---
+++ StockSquad/StockSquadApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockSquadApp.swift
+//  StockSquad
+//
+//  Created by Tuntufye on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct StockSquadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
